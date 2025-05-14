3e0a157d8d7239d48cf5edd4246b95483c95a6b2
nathan86400 <nathanlong86000@gmail.com>
2025-05-14 15:06:53 +0200

    gestion des fichier/dossier + debut generateur html et .css

---
+++ System/Generator/html.cs
@@ -0,0 +1,31 @@
+using System;
+
+public async Task HTMLGenerator(string FileTag, NewsAPIService newAPI, UserInterfacesErreur Error) {
+    string html =$@"
+        <!DOCTYPE html>
+        <html lang='fr'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>{FileTag}</title>
+            <link rel='stylesheet' href='../style/style.css'>
+        </head>
+        <body>
+            <div class='container'>
+        ";
+
+        html += @"
+            </div>
+        </body>
+        </html>
+        ";
+
+        error.ErreurDirectoryPath("output");
+        if (error.ErreurFileExists($"output/{fileName}.html"))
+        {
+            File.WriteAllText($"output/{fileName}.html", html);
+            Console.WriteLine("Fichier créer avec succès");
+        }
+}
+
+ 
